71c2b6a05b33117eb35b7076b06d857ee6cd064d
Adam Donaghy <donaghyam@gmail.com>
2022-05-10 11:04:16 -0500

    fixed security key issue

--- rater_project/settings.py
+++ rater_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^fg_ea8&82d+0p!fre6+_uqd@g)nw^7rrsici(gt(=b5z(8#m0'
+SECRET_KEY = os.environ.get('MY_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
